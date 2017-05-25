aea8cf31dd62bff0ac027ebcc2918325a1b252db
Kendal Droddy <kdroddy001@gmail.com>
2017-05-25 18:33:31 -0500

    added js files

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+//Variables used for url generation
+// console.log("meow");
+// var urlFirst = "http://api.openweathermap.org/data/2.5/weather?";
+// var urlSecond = "&appid=fa7d80c48643dfadde2cced1b1be6ca1";
+var urlThird = "https://developer.nps.gov/api/v0/alerts?parkCode=yose,yell";
+
+
+var req = new XMLHttpRequest();
+req.open("GET", urlThird, false);
+
+req.setRequestHeader(Authorization: "6A543129-1D03-4C0D-95FC-0D8010869FFC");
+
+var response = JSON.parse(req.responseText);
+console.log(response);
+req.send(null);
+
+
+// var requestMethod = "GET";
+// var queryURL = "https://developer.nps.gov/api/v0/";
+// var queryString = "alerts?parkCode=yose";
+// var apiKey = "Authorization=6A543129-1D03-4C0D-95FC-0D8010869FFC";
+
+// var request = new XMLHttpRequest();
+// request.open(requestMethod, queryURL, true);
+// request.setRequestHeader(Authorization: {6A543129-1D03-4C0D-95FC-0D8010869FFC});
+// request.addEventListener("load", function() {
+//     var response = JSON.parse(request.responseText);
+//     console.log(response);
+// });
+
+// request.send(null);
