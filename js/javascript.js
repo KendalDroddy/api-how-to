e79bb9d36c64bea1cc518fa6df9708541a8d03f6
Kendal Droddy <kdroddy001@gmail.com>
2017-05-26 18:37:52 -0500

    making sure everything is uploaded

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@
 // console.log("meow");
 // var urlFirst = "http://api.openweathermap.org/data/2.5/weather?";
 // var urlSecond = "&appid=fa7d80c48643dfadde2cced1b1be6ca1";
-// var urlThird = "https://developer.nps.gov/api/v0/alerts?parkCode=yose,yell";
+ var urlThird = "https://developer.nps.gov/api/v0/alerts?parkCode=yose,yell";
 
 
 // var req = new XMLHttpRequest();
@@ -20,7 +20,7 @@ var queryURL = "https://developer.nps.gov/api/v0/";
 var queryString = "alerts?parkCode=yose";
 
 var request = new XMLHttpRequest();
-request.open(requestMethod, queryURL, false);
+request.open(requestMethod, urlThird , true);
 request.setRequestHeader('Authorization', '6A543129-1D03-4C0D-95FC-0D8010869FFC');
 request.addEventListener("load", function() {
     var response = JSON.parse(request.responseText);
